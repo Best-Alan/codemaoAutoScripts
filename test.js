35c1af0a8c3fca3401f37b465cdf7af067ea1e59
Best_alan <114794097+Best-Alan@users.noreply.github.com>
2024-10-04 17:01:17 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 let headers = {
   "Content-Type": "application/json",
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0",
