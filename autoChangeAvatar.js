5c7b3df0d017f071832265b6388545bdde91cafb
Best_alan <114794097+Best-Alan@users.noreply.github.com>
2024-10-05 22:59:46 +0800

    Create autoChangeAvatar.js

---
+++ autoChangeAvatar.js
@@ -0,0 +1,42 @@
+let headers = {
+  "Content-Type": "application/json",
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0",
+};
+
+let MONUMENT_VALLEY_AVATARS_URLS = JSON.parse(process.env.MONUMENT_VALLEY_AVATARS_URLS)
+
+Array.prototype.randomItem = function() {
+  return this[Math.floor(Math.random() * this.length)]
+}
+
+let avatarUrl = MONUMENT_VALLEY_AVATARS_URLS.randomItem()
+console.log(avatarUrl);
+
+(async function() {
+  await fetch(`https://api.codemao.cn/tiger/v3/web/accounts/login`, {
+    method: "POST",
+    body: JSON.stringify({
+      identity: process.env.IDENTITY,
+      password: process.env.PASSWORD,
+      pid: "65edCTyg", // 写死的, 不会变动
+      agreement_ids: [-1]
+    }),
+    headers: headers
+  }).then(async (res) => {
+    //if (res.ok)
+    let resJson = await res.json()
+    console.log(resJson)
+    headers.cookie = 'authorization=' + resJson.auth.token
+  })
+
+  fetch(`https://api.codemao.cn/tiger/v3/web/accounts/info`, {
+    method: "PATCH",
+    body: JSON.stringify({
+      "avatar_url": avatarUrl
+    }),
+    headers: headers
+  }).then(async (res) => {
+    //if (res.ok)
+    console.log(res.status, await resText)
+  })
+})()
