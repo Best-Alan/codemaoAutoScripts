114024396fe6f52096e4d138d4ce384c9621b84b
Best_alan <114794097+Best-Alan@users.noreply.github.com>
2024-10-06 09:37:48 +0800

    Update websocketTest.js

--- websocketTest.js
+++ websocketTest.js
@@ -1,3 +1,6 @@
+let ws = require('ws')
+console.log(ws)
+
 let headers = {
   "Content-Type": "application/json",
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0",
